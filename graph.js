b882bccff79f27c7bce47908ce39207032956c89
christosci <christoskok@gmail.com>
2018-12-20 21:30:16 -0600

    improved perfomance

--- graph.js
+++ graph.js
@@ -1,168 +1,95 @@
-var svg = d3.select("svg"),
-  margin = { top: 20, right: 80, bottom: 110, left: 80 },
-  margin2 = { top: 430, right: 80, bottom: 30, left: 80 },
-  width = +svg.attr("width") - margin.left - margin.right,
-  height = +svg.attr("height") - margin.top - margin.bottom,
-  height2 = +svg.attr("height") - margin2.top - margin2.bottom;
+var chartDiv = document.getElementById("chart");
+var svg = d3.select(chartDiv).append("svg");
 
-var parseDate = d3.timeParse("%Y-%m-%d");
+var raw_data;
+var regression;
 
-var x = d3.scaleTime().range([0, width]),
-  x2 = d3.scaleTime().range([0, width]),
-  y = d3.scaleLog().range([height, 0]).base(10),
-  y2 = d3.scaleLinear().range([height2, 0]);
+function render() {
+  d3.selectAll("svg > *").remove();
+  var margin = { top: 20, right: 25, bottom: 30, left: 50 },
+    width = chartDiv.clientWidth - margin.left - margin.right,
+    height = chartDiv.clientHeight - margin.top - margin.bottom;
 
-var xAxis = d3.axisBottom(x),
-  xAxis2 = d3.axisBottom(x2),
-  yAxis = d3.axisLeft(y).ticks(7).tickFormat(function(d){
-    return d3.format("($.2f")(d);
-  });
+  // set the ranges
+  var x = d3.scaleTime().range([0, width]);
+  var y = d3
+    .scaleLog()
+    .range([height, 0])
+    .base(10)
+    .clamp(true);
 
-var brush = d3
-  .brushX()
-  .extent([[0, 0], [width, height2]])
-  .on("brush end", brushed);
-
-var zoom = d3
-  .zoom()
-  .scaleExtent([1, Infinity])
-  .translateExtent([[0, 0], [width, height]])
-  .extent([[0, 0], [width, height]])
-  .on("zoom", zoomed);
-
-var line = d3
-  .line()
-  .x(function(d) {
-    return x(d.date);
-  })
-  .y(function(d) {
-    return y(d.price);
-  });
-
-var line2 = d3
-  .line()
-  .x(function(d) {
-    return x2(d.date);
-  })
-  .y(function(d) {
-    return y2(d.price);
-  });
-
-var clip = svg
-  .append("defs")
-  .append("svg:clipPath")
-  .attr("id", "clip")
-  .append("svg:rect")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("x", 0)
-  .attr("y", 0);
-
-var Line_chart = svg
-  .append("g")
-  .attr("class", "focus")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  .attr("clip-path", "url(#clip)");
-
-var focus = svg
-  .append("g")
-  .attr("class", "focus")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var context = svg
-  .append("g")
-  .attr("class", "context")
-  .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-d3.json("close.json", function(error, data) {
-  if (error) throw error;
-
-  var data = Object.keys(data.bpi).map(function(date) {
-    return {
-      date: parseDate(date),
-      price: data.bpi[date]
-    };
-  });
-
-  x.domain(
-    d3.extent(data, function(d) {
-      return d.date;
+  var valueline = d3
+    .line()
+    .x(d => {
+      return x(d.x);
     })
-  );
-  y.domain([
-    0.01,
-    1000000
-  ]);
-  x2.domain(x.domain());
-  y2.domain([
-    0,
-    d3.max(data, function(d) {
-      return d.price;
-    })
-  ]);
+    .y(d => {
+      return y(d.y);
+    });
 
-  focus
+  svg
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+  var chart = svg
     .append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  focus
-    .append("g")
-    .attr("class", "axis axis--y")
-    .call(yAxis);
+  // Scale the range
+  x.domain([regression[0].x, regression[regression.length - 1].x]);
+  y.domain([100, 10000000]);
 
-  Line_chart.append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
-
-  context
+  // Add the valueline path.
+  chart
     .append("path")
-    .datum(data)
+    .data([raw_data])
     .attr("class", "line")
-    .attr("d", line2);
+    .attr("d", valueline);
 
-  context
+  // Add the valueline2 path.
+  chart
+    .append("path")
+    .data([regression])
+    .attr("class", "line")
+    .style("stroke", "red")
+    .attr("d", valueline);
+
+  // Add the X Axis
+  chart
     .append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height2 + ")")
-    .call(xAxis2);
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
-  context
+  // Add the Y Axis
+  chart
     .append("g")
-    .attr("class", "brush")
-    .call(brush)
-    .call(brush.move, x.range());
-
-  svg
-    .append("rect")
-    .attr("class", "zoom")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .call(zoom);
-});
-
-function brushed() {
-  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
-  var s = d3.event.selection || x2.range();
-  x.domain(s.map(x2.invert, x2));
-  Line_chart.select(".line").attr("d", line);
-  focus.select(".axis--x").call(xAxis);
-  svg
-    .select(".zoom")
+    .attr("class", "axisSteelBlue")
     .call(
-      zoom.transform,
-      d3.zoomIdentity.scale(width / (s[1] - s[0])).translate(-s[0], 0)
+      d3
+        .axisLeft(y)
+        .ticks(4, "s")
+        .tickFormat(d3.format(".0s"))
     );
 }
 
-function zoomed() {
-  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
-  var t = d3.event.transform;
-  x.domain(t.rescaleX(x2).domain());
-  Line_chart.select(".line").attr("d", line);
-  focus.select(".axis--x").call(xAxis);
-  context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
+function parse_json(data) {
+  var parseDate = d3.timeParse("%s");
+  return data.values.map(point => {
+    return {
+      x: parseDate(point.x),
+      y: point.y
+    };
+  });
 }
+
+d3.queue()
+  .defer(d3.json, "data/transactions.json")
+  .defer(d3.json, "data/regressions/transactions_power.json")
+  .await((error, data1, data2) => {
+    if (error) throw error;
+    raw_data = parse_json(data1);
+    regression = parse_json(data2);
+
+    render();
+    window.addEventListener("resize", render);
+  });
