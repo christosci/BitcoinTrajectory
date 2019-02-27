70091b1bded415ec70cfdf210158866f6586b213
christosci <christoskok@gmail.com>
2019-02-27 09:09:51 -0600

    do_curve_fit now calculates confidence limits from pcov + more regression formulas

--- cron/regression.py
+++ cron/regression.py
@@ -1,50 +1,56 @@
 import numpy as np
+import math
 from scipy.optimize import curve_fit
-import json
+from scipy.stats.distributions import t
+from constants import *
+from process_data import *
+
+def get_cl_from_se(se, z_score):
+    return np.asarray(se) * z_score
+
+def square(x):
+    return x**2
 
 def power(fx, a, b):
-    return a*(fx)**b
+    return a*fx**b
 
-def get_r2(f, x, y, popt):
-    ss_res = np.dot((y - f(x, *popt)),(y - f(x, *popt)))
-    ymean = np.mean(y)
-    ss_tot = np.dot((y-ymean),(y-ymean))
-    return 1-ss_res/ss_tot
+def generalized_metcalfe(u, a=1.51, b=1.69):
+    """
+    u - active addresses
+    """
+    return math.exp(a)*(u**b)
 
-def do_curve_fit(f, x, y):
-    popt, pcov = curve_fit(power, x, y)
-    r2 = get_r2(power, x, y, popt)
-    return popt, r2
+def m2(n, s):
+    """
+    n - transactions or active addresses
+    s - current supply
+    """
+    return 100*(n**1.5/s)
 
-def json_to_xy(filepath, x_start, x_stop):
-    """
-    Parses JSON xy values from blockchain.com charts API.
-    :returns: raw y values as y[] and their indices as x[].
-    """
-    y = []
-    with open(filepath) as json_data:
-        data = json.load(json_data)
-    for v in data['values']:
-        if (x_start <= v['x'] <= x_stop):
-            y.append(v['y'])
-    x = range(len(y))
-    return x, y
+def log(x, a, b):
+    if x == 0: return 0
+    return 10**(a*np.log(x)-b)
 
-def xy_to_json(filepath, r2, popt, values):
-    """
-    Writes r2 and xy values of regression line to JSON file.
-    """
-    regression_data = {'r2': r2, 'popt': popt, 'values': values}
-    with open(filepath, 'w') as write_file:
-        json.dump(regression_data, write_file, indent=2)
+def do_curve_fit(func, x, y, alpha=0.05):
+    x = np.array(x, dtype=np.int64)
+    y = np.array(y, dtype=np.int64)
+    coeffs, pcov = curve_fit(func, x, y)
+    n = len(y)
+    p = len(coeffs)
+    dof = max(0, n - p) # number of degrees of freedom
+    # student-t value for the dof and confidence level
+    tval = t.ppf(1.0-alpha/2.0, dof) 
+    cl = np.multiply(np.sqrt(np.diag(pcov)), tval)
+    return coeffs, cl
 
-def regression_to_xy_dict(f, popt, x_start, x_stop, x_step):
-    """
-    :returns: dict of xy values based on given regression parameters.
-    """
-    values = []
-    for fx, x in enumerate(range(x_start, x_stop, x_step)):
-        y = int(f(fx, popt[0], popt[1]))
-        if y == 0: continue
-        values.append({'x': x, 'y': y})
-    return values
+def create_regression(input_filepath, output_filepath, func):
+    x, y = json_to_xy(input_filepath, START_TIMESTAMP, END_TIMESTAMP)
+    x_stop = START_TIMESTAMP + REGRESSION_TIMESPAN
+    coeffs, cl = do_curve_fit(func, x, y)
+    values = regression_to_xy_dict(START_TIMESTAMP, x_stop, REGRESSION_X_STEP, func, coeffs, cl)
+    xy_to_json(output_filepath, coeffs.tolist(), values)
+
+def plot_function(output_filepath, x_start, func, coeffs):
+    x_stop = START_TIMESTAMP + REGRESSION_TIMESPAN
+    values = regression_to_xy_dict(x_start, x_stop, REGRESSION_X_STEP, log, coeffs)
+    xy_to_json(output_filepath, coeffs.tolist(), values)
