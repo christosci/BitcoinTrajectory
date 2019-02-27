64694baa996e501d2d5165db5740b968135ad300
christosci <christoskok@gmail.com>
2019-02-27 09:05:01 -0600

    script to minify data for thumbnails on the front page first commit

---
+++ cron/minify_data.py
@@ -0,0 +1,55 @@
+import json
+from os import path
+import os
+import time
+import calendar
+from constants import *
+from process_data import read_from_json, write_to_json
+
+def minify_data(values_to_skip):
+    for f in data_filenames:
+        new_values = []
+        data = read_from_json(path.join(DATA_PATH, f))
+        for i in range(0, len(data['values']), values_to_skip):
+            new_values.append(data['values'][i])
+        data['values'] = new_values
+        write_to_json(path.join(DATA_MIN_PATH, f), data)
+
+def minify_normalized_data(values_to_skip):
+    for f in normalized_filenames:
+        new_values = []
+        data = read_from_json(path.join(NORMALIZED_PATH, f))
+        for i in range(0, len(data['values']), values_to_skip):
+            new_values.append(data['values'][i])
+        data['values'] = new_values
+        write_to_json(path.join(NORMALIZED_MIN_PATH, f), data)
+
+def minify_regressions():
+    for f in regression_filenames:
+        new_values = []
+        data = read_from_json(path.join(REGRESSIONS_PATH, f))
+        new_values = [v for v in data['values'] if START_TIMESTAMP < v['x'] < current_time]
+        data['values'] = new_values
+        write_to_json(path.join(REGRESSIONS_MIN_PATH, f), data)
+
+def minify():
+    current_time = int(calendar.timegm(time.gmtime()))
+    data_filenames = []
+    normalized_filenames = []
+    regression_filenames = []
+
+    for f in os.listdir(DATA_PATH):
+    if f.endswith(".json"): 
+            data_filenames.append(f)
+
+    for f in os.listdir(NORMALIZED_PATH):
+    if f.endswith(".json"): 
+            normalized_filenames.append(f)
+
+    for f in os.listdir(REGRESSIONS_PATH):
+    if f.endswith(".json"): 
+            regression_filenames.append(f)
+
+    minify_data(7)
+    minify_normalized_data(7)
+    minify_regressions()
