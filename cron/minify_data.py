9528b7474f5f265c5b0252c8bbad544e5d629a14
christosci <christoskok@gmail.com>
2019-03-20 04:01:56 -0500

    add minify normalized data

--- cron/minify_data.py
+++ cron/minify_data.py
@@ -1,8 +1,6 @@
 import json
 from os import path
 import os
-import time
-import calendar
 from constants import *
 from process_data import read_from_json, write_to_json
 
@@ -28,28 +26,27 @@ def minify_regressions():
     for f in regression_filenames:
         new_values = []
         data = read_from_json(path.join(REGRESSIONS_PATH, f))
-        new_values = [v for v in data['values'] if START_TIMESTAMP < v['x'] < current_time]
+        new_values = [v for v in data['values'] if START_TIMESTAMP < v['x'] < CURRENT_TIME]
         data['values'] = new_values
         write_to_json(path.join(REGRESSIONS_MIN_PATH, f), data)
 
-def minify():
-    current_time = int(calendar.timegm(time.gmtime()))
-    data_filenames = []
-    normalized_filenames = []
-    regression_filenames = []
 
-    for f in os.listdir(DATA_PATH):
+data_filenames = []
+normalized_filenames = []
+regression_filenames = []
+
+for f in os.listdir(DATA_PATH):
     if f.endswith(".json"): 
             data_filenames.append(f)
 
-    for f in os.listdir(NORMALIZED_PATH):
+for f in os.listdir(NORMALIZED_PATH):
     if f.endswith(".json"): 
             normalized_filenames.append(f)
 
-    for f in os.listdir(REGRESSIONS_PATH):
+for f in os.listdir(REGRESSIONS_PATH):
     if f.endswith(".json"): 
             regression_filenames.append(f)
 
-    minify_data(7)
-    minify_normalized_data(7)
-    minify_regressions()
+minify_data(7)
+minify_normalized_data(7)
+minify_regressions()
