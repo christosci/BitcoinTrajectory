dd5bfa021d61544bfe97c8411c12e89535a8ff4b
christosci <christoskok@gmail.com>
2019-05-22 16:42:47 -0500

    refactoring

--- cron/cron.py
+++ cron/cron.py
@@ -1,40 +1,55 @@
+import sys
 import json
 from constants import *
 from calculate import *
 from process_data import *
 
-def main():
-    # fetch data and format it
-    # fetch_json()
-    # format_coinmetrics_data(ADDRESSES)
-    # format_coinmetrics_data(REALIZEDCAP)
-    # for info in DATA_INFO:
-    #     remove_zero_values(info['path'])
+def fetch():
+    """
+    Fetch data and format it.
+    """
+    fetch_json() # fetch json files
+    # format everything
+    format_coinmetrics_data(ADDRESSES)
+    format_coinmetrics_data(REALIZEDCAP)
+    for info in DATA_INFO:
+        remove_zero_values(info['path'])
 
-    # # normalize data
-    # normalize_data(TRANSACTIONS, TRANSACTIONS_SQUARED, square)
-    # normalize_data(ADDRESSES, ADDRESSES_GENMETCALFE, generalized_metcalfe)
-    # # m2
-    # get_m2(TRANSACTIONS, SUPPLY, TRANSACTIONS_M2, m2)
-
-    # import minify_data
+def normalize():
+    """
+    Normalize data - needs to be done in a routine basis after fetching data.
+    """
+    normalize_data(TRANSACTIONS, TRANSACTIONS_SQUARED, square)
+    normalize_data(ADDRESSES, ADDRESSES_GENMETCALFE, generalized_metcalfe)
+    create_stock_to_flow(MONTHLY_SUPPLY, STOCK_TO_FLOW, stock_to_flow)
     
-    # transactions squared
-    # create_regression(TRANSACTIONS, TRANSACTIONS_POWER_SQUARED, power)
-    # normalize_data(TRANSACTIONS_POWER_SQUARED, TRANSACTIONS_POWER_SQUARED, square)
+def regress():
+    """
+    Perform regression analysis over data.
+    """
+    # metcalfe's law
+    create_regression(TRANSACTIONS, TRANSACTIONS_POWER_SQUARED, power)
+    normalize_data(TRANSACTIONS_POWER_SQUARED, TRANSACTIONS_POWER_SQUARED, square)
 
-    # addresses generalized metcalfe
-    # create_regression(ADDRESSES, ADDRESSES_POWER_GENMETCALFE, power)
-    # normalize_data(ADDRESSES_POWER_GENMETCALFE, ADDRESSES_POWER_GENMETCALFE, generalized_metcalfe)
+    # generalized metcalfe's law
+    create_regression(ADDRESSES, ADDRESSES_POWER_GENMETCALFE, power)
+    normalize_data(ADDRESSES_POWER_GENMETCALFE, ADDRESSES_POWER_GENMETCALFE, generalized_metcalfe)
 
-    # Trolololo
-    # plot_function(TROLOLOLO_LOG, 1231459200, log, np.asarray([2.66167155005961, 17.9183761889864]))
-
+def plot():
+    """
+    Plot functions with given constants and coefficients.
+    """
+    # Trolololo's logarithmic regression
+    plot_function(TROLOLOLO_LOG, 1231459200, log, np.asarray([2.66167155005961, 17.9183761889864]))
     # Power law
-    plot_function(POWER_LAW, 1230940800, power, np.asarray([3.4896e-18, 5.9762], dtype=np.float))
-    # normalize_data(POWER_LAW, POWER_LAW, oneThousandth)
+    plot_function(POWER_LAW, 1230940800, power, np.asarray([3.4896e-18, 5.9762], dtype=np.float))    
 
-    # normalize_data_xy(SUPPLY, STOCK_TO_FLOW, stock_to_flow)
 
 if __name__ == "__main__":
-    main()
+    for arg in sys.argv[1:]:
+        if arg == 'fetch': fetch()
+        elif arg == 'normalize': normalize()
+        elif arg == 'regress': regress()
+        elif arg == 'plot': plot()
+
+    import minify_data
