e4152a47cdf296e4af4b152b5ac09a9ed484ab64
christosci <christoskok@gmail.com>
2019-05-30 20:47:40 -0500

    added some more constants

--- cron/constants.py
+++ cron/constants.py
@@ -1,14 +1,12 @@
 from os import path
 import calendar
 import time
-from process_data import *
 
 START_TIMESTAMP = 1279324800
 END_TIMESTAMP = 1519603200
 REGRESSION_X_STEP = 30 # days
 REGRESSION_TIMESPAN = 631139040 # 20 years
 CURRENT_TIME = int(calendar.timegm(time.gmtime()))
-DECAMINUTES = 52560
 
 DATA_PATH = path.abspath(path.join(path.dirname(__file__), '..', 'data'))
 DATA_MIN_PATH = path.abspath(path.join(path.dirname(__file__), '..', 'data-min'))
@@ -21,7 +19,8 @@ NORMALIZED_MIN_PATH = path.join(DATA_MIN_PATH, 'normalized')
 
 # static data
 HALVINGS = path.join(STATIC_PATH, 'halvings.json')
-HALVINGS_ARR = json_to_x(HALVINGS)
+BLKHDRS_RAW = path.join(STATIC_PATH, 'blkhdrs')
+BLKHDRS = path.join(STATIC_PATH, 'blockchain_headers.csv')
 
 # raw data
 ADDRESSES = path.join(DATA_PATH, 'addresses.json')
@@ -30,6 +29,8 @@ PRICE = path.join(DATA_PATH, 'price.json')
 REALIZEDCAP = path.join(DATA_PATH, 'realizedcap.json')
 SUPPLY = path.join(DATA_PATH, 'supply.json')
 TRANSACTIONS = path.join(DATA_PATH, 'transactions.json')
+MONTHLY_SUPPLY = path.join(DATA_PATH, 'monthly_supply.json')
+INTEREST = path.join(DATA_PATH, 'interest.json')
 
 # regression data
 TRANSACTIONS_POWER_SQUARED = path.join(REGRESSIONS_PATH, 'transactions_power_squared.json')
@@ -42,6 +43,7 @@ TRANSACTIONS_SQUARED = path.join(NORMALIZED_PATH, 'transactions_squared.json')
 ADDRESSES_GENMETCALFE = path.join(NORMALIZED_PATH, 'addresses_genmetcalfe.json')
 TRANSACTIONS_M2 = path.join(NORMALIZED_PATH, 'transactions_m2.json')
 STOCK_TO_FLOW = path.join(NORMALIZED_PATH, 'stock_flow.json')
+INTEREST_SCALED = path.join(NORMALIZED_PATH, 'interest_scaled.json')
 
 # API URLs
 BLOCKCHAIN_URL = 'https://api.blockchain.info/charts/'
