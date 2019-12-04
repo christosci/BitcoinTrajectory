d4513c75af210238c8a39e01a95e892ed027ec89
christosci <christoskok@gmail.com>
2019-12-04 01:04:49 -0600

    add r2, fear and greed, refactor for readability

--- cron/cron.py
+++ cron/cron.py
@@ -15,6 +15,7 @@ def fetch():
     # format everything
     format_coinmetrics_data(ADDRESSES)
     format_coinmetrics_data(REALIZEDCAP)
+    format_fear_greed(FEAR_GREED)
     for info in DATA_INFO:
         remove_zero_values(info['path'])
 
@@ -22,34 +23,115 @@ def normalize():
     """
     Normalize data - needs to be done in a routine basis after fetching data.
     """
-    apply(TRANSACTIONS, TRANSACTIONS_SQUARED, square)
-    apply(ADDRESSES, ADDRESSES_GENMETCALFE, generalized_metcalfe)
-    create_stock_to_flow(MONTHLY_SUPPLY, STOCK_TO_FLOW, stock_to_flow)
-    apply(INTEREST, INTEREST_SCALED, scale, get_max_y(PRICE)/100)
-    create_log_returns(PRICE, DAILY_LOG_RETURNS, log_return)
-    apply2(TRANSACTIONS_SQUARED, SUPPLY, METCALFE_PRICE, np.divide)
-    apply2(PRICE, METCALFE_PRICE, METCALFE_MULTIPLE, np.divide)
+    apply(
+        input_filepath = TRANSACTIONS, 
+        output_filepath = TRANSACTIONS_SQUARED, 
+        func = square
+    )
+    apply(
+        input_filepath = ADDRESSES, 
+        output_filepath = ADDRESSES_GENMETCALFE, 
+        func = generalized_metcalfe
+    )
+    create_stock_to_flow(
+        supply_path = MONTHLY_SUPPLY, 
+        output_filepath = STOCK_TO_FLOW, 
+        func = stock_to_flow
+    )
+    apply(
+        INTEREST, 
+        INTEREST_SCALED, 
+        scale, 
+        get_max_y(PRICE)/100
+    )
+    create_log_returns(
+        input_filepath = PRICE, 
+        output_filepath = DAILY_LOG_RETURNS,
+        func = log_return
+    )
+    apply2(
+        a_path = TRANSACTIONS_SQUARED, 
+        b_path = SUPPLY, 
+        output_filepath = METCALFE_PRICE, 
+        func = np.divide
+    )
+    apply2(
+        a_path = PRICE, 
+        b_path = METCALFE_PRICE,
+        output_filepath = METCALFE_MULTIPLE,
+        func = np.divide
+    )
     
 def regress():
     """
     Perform regression analysis over data.
     """
     # metcalfe's law
-    create_regression(TRANSACTIONS, TRANSACTIONS_POWER_SQUARED, power)
-    apply(TRANSACTIONS_POWER_SQUARED, TRANSACTIONS_POWER_SQUARED, square)
+    create_regression(
+        input_filepath = TRANSACTIONS, 
+        output_filepath = TRANSACTIONS_POWER_SQUARED,
+        x_stop = START_TIMESTAMP + REGRESSION_TIMESPAN,
+        func = power
+    )
+    apply(
+        input_filepath = TRANSACTIONS_POWER_SQUARED,
+        output_filepath = TRANSACTIONS_POWER_SQUARED,
+        func = square
+    )
     
     # generalized metcalfe's law
-    create_regression(ADDRESSES, ADDRESSES_POWER_GENMETCALFE, power)
-    apply(ADDRESSES_POWER_GENMETCALFE, ADDRESSES_POWER_GENMETCALFE, generalized_metcalfe)
+    create_regression(
+        input_filepath = ADDRESSES, 
+        output_filepath = ADDRESSES_POWER_GENMETCALFE,
+        x_stop = START_TIMESTAMP + REGRESSION_TIMESPAN,
+        func = power
+    )
+    apply(
+        input_filepath = ADDRESSES_POWER_GENMETCALFE, 
+        output_filepath = ADDRESSES_POWER_GENMETCALFE,
+        func = generalized_metcalfe
+    )
 
 def plot():
     """
     Plot functions with given constants and coefficients.
     """
     # Trolololo's logarithmic regression
-    plot_function(TROLOLOLO_LOG, 1231459200, log, np.asarray([2.66167155005961, 17.9183761889864]))
+    plot_function(
+        output_filepath = TROLOLOLO_LOG, 
+        x_start = 1231459200,
+        x_stop = 1798761600,
+        func = log, 
+        coeffs = np.asarray([2.66167155005961, 17.9183761889864])
+    )
+
     # Power law
-    plot_function(POWER_LAW, 1230940800, power, np.asarray([3.4896e-18, 5.9762], dtype=np.float))    
+    plot_function(
+        output_filepath = POWER_LAW, 
+        x_start = 1230940800, 
+        x_stop = 1910463840,
+        func = power,
+        coeffs = np.asarray([3.4896e-18, 5.9762], dtype=np.float)
+    )
+
+def r2():
+    trolololo = get_r2(
+        x_start = 1231459200,
+        true = read_from_json(PRICE)['values'],
+        func = log,
+        coeffs = np.asarray([2.66167155005961, 17.9183761889864])
+    )
+
+    add_element(TROLOLOLO_LOG, {'r2': trolololo})
+
+    power_law = get_r2(
+        x_start = 1230940800,
+        true = read_from_json(PRICE)['values'],
+        func = power,
+        coeffs = np.asarray([3.4896e-18, 5.9762], dtype=np.float)
+    )
+
+    add_element(POWER_LAW, {'r': power_law})
 
 
 if __name__ == "__main__":
@@ -59,3 +141,4 @@ if __name__ == "__main__":
         elif arg == 'regress': regress()
         elif arg == 'plot': plot()
         elif arg == 'minify': minify()
+        elif arg == 'r2': r2()
