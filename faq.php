402a9b6d03a2acb2b408387e4973ee6afcd5368a
christosci <christoskok@gmail.com>
2019-12-04 01:10:42 -0600

    fix writing

--- faq.php
+++ faq.php
@@ -11,14 +11,17 @@
         </div>
         <div id="collapse1" class="panel-collapse collapse in">
           <div class="panel-body">
-            <p>Ever since Bitcoin's inception over a decade ago, people have sought 
-            to predict the trajectory of its value using a wide array of statistical models.</p>
+            <p>People who are first introduced to Bitcoin often ask questions like "does bitcoin have any value?" 
+            or "what will it be worth five to ten years from now?"</p>
+
+            <p>Statisticians, Data scientists and econometricians have sought to answer these questions by constructing 
+            various statistical models. The results of which are often presented in the form of visualizations.</p>
 
             <p>Given that Bitcoin is a constantly evolving digital asset, it is imperative to present the latest network and exchange data
-            in statistical visualizations, such that we can easily judge the accuracy of a statistical model.
-            However, many visualizations posted in websites, forums, and research papers
+            in visualizations, such that we can easily judge the accuracy of a model.<p>
+            <p>However, many visualizations posted in websites, forums, and research papers
             are no longer maintained by their authors.</p>
-
+            
             <p>BitcoinTrajectory.com seeks to solve this problem by providing a curated list of visualizations, all in one place, 
             all updated on a daily basis.</p>
           </div>
@@ -47,9 +50,8 @@
         <div id="collapse3" class="panel-collapse collapse">
           <div class="panel-body">
             <p>Bitcoin network and exchange data are updated on a daily basis sometime around midnight UTC.</p>
-            <p>In an effort to show their predictive power (or lack thereof), regression coefficients originally posted 
-              elsewhere are never recalculated. In cases where regression coefficients have not been posted (such as Metcalfe's Law), 
-              we have performed our own regression calculations on daily values from from Jul 17, 2010 to Feb 26, 2018.
+            <p>In an effort to show each model's predictive power (or lack thereof) by means of cross-validation, 
+            model parameters are never updated.
             </p>
             <p>
               New visualizations are added on a regular basis, so be sure to check back every now and then.
@@ -60,27 +62,10 @@
       <div class="panel panel-default">
         <div class="panel-heading">
           <h4 class="panel-title">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">Why do you not display coefficients of determination(R&sup2;) for any of your regression models?</a>
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">How can I support this project?</a>
           </h4>
         </div>
         <div id="collapse4" class="panel-collapse collapse">
-          <div class="panel-body">
-            <p>
-              While there are several nonlinear regression models out there that display R&sup2; values, those R&sup2; values are misleading and 
-              should not be trusted. It is important to understand that R&sup2; is a goodness-of-fit measure for linear regressions only.</p>
-            <p>
-              In lieu of R&sup2;, some charts on this website display 95% confidence bands to help you judge the goodness-of-fit.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h4 class="panel-title">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">How can I support this project?</a>
-          </h4>
-        </div>
-        <div id="collapse5" class="panel-collapse collapse">
           <div class="panel-body">
             <p>There are no ads on this website. This project is supported solely by donations from individuals like you.</p>
             <p>If you find the content on this website useful, please consider donating BTC to the address found at the bottom of the page. Thank you.</p>
